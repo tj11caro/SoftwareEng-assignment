d24292a9f1b2ef2408bc031cf6a55a141b7c63e6
Nick Marotta <na31maro@siena.edu>
2018-03-25 19:51:44 -0400

    Stable single page app functionality
    I just finished making the site a single page app. The way you need to reference URLS  is a little different now.
    http://oraserv.cs.siena.edu/~perm_team1_2017/nick/#/     <- This is the homepage
    http://oraserv.cs.siena.edu/~perm_team1_2017/nick/#/admin <- This brings you to admin.html
    http://oraserv.cs.siena.edu/~perm_team1_2017/nick/#/home <- This brings you to user.html
    The key for where each of these URL keywords resolves to can be found in `project/js/app.js`
    Any new page added needs to have its route added to that file (just follow the other examples, its really easy. And then the navigation links would need to be updated as well. but now instead of putting `admin.html` in an href, you would write `#/admin` in the href. Any questions feel free to text me.
    I am sending a pull request to @bebber, he's going to review it and merge it in when he sees fit.

--- project/js/app.js
+++ project/js/app.js
@@ -7,41 +7,28 @@ app.config(function ($routeProvider) {
         .when("/admin", {
             templateUrl: "views/admin/admin.html"
         })
+        .when("/home", {
+            templateUrl: "views/user/user.html"
+        })
+        .when("/select-prospects", {
+            templateUrl: "views/user/selectProspects.html"
+        })
+        .when("/view-prospects", {
+            templateUrl: "views/user/userProspects.html"
+        })
+        .when("/advanced-search", {
+            templateUrl: "views/user/advancedSearch.html"
+        })
         .when("/admin/view-users", {
             templateUrl: "views/admin/admin-users.html"
         })
-        .when("/green", {
-            templateUrl: "green.htm"
+        .when("/admin/reports", {
+            templateUrl: "views/admin/admin-reports.html"
         })
-        .when("/blue", {
-            templateUrl: "blue.htm"
+        .when("/admin/prospects", {
+            templateUrl: "views/admin/admin-prospects.html"
+        })
+        .when("/admin/import", {
+            templateUrl: "views/admin/admin-import.html"
         });
-});
-
-app.service('user', function() {
-   
-    var username; 
-    var loggedIn = false; 
-    var admin = false; 
-    
-    this.setUsername = function(name) {
-        username = name; 
-    };
-    this.getName = function() {
-      return username;   
-    };
-    this.isUserLoggedIn = function() {
-        return loggedIn; 
-    };
-    this.userLoggedIn = function() {
-        loggedIn = true; 
-    };
-    this.isAdmin = function() {
-        return admin; 
-    };
-    this.setAdmin = function(bool) {
-        admin = bool; 
-    };
-    
-    
 });
